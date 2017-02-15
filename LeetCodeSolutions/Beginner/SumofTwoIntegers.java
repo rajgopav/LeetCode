968e7a33e18e29f3bba6cbf80894715710a74225
Varun <rajgopav@onid.oregonstate.edu>
2017-02-14 22:21:24 -0800

    02/14/2017: New files

---
+++ LeetCodeSolutions/Beginner/SumofTwoIntegers.java
@@ -0,0 +1,18 @@
+/**
+ * @author Varun
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ *
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ */
+public class SumofTwoIntegers {
+	
+	public int getSum(int a, int b) {
+	    while(b!=0) {
+	    	int c = a&b;
+	    	a ^= b;
+	    	b = c << 1;
+	    }
+	    return a;
+	}
+}
